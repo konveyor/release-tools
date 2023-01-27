34bc41ea8afdc176e023def19407205f26da1aa2
David Zager <david.j.zager@gmail.com>
2023-01-27 14:43:02 -0500

    Address review comments
    - Change action description
    - Drop anonymous function
    
    Signed-off-by: David Zager <david.j.zager@gmail.com>

--- cmd/verify-pr/verify-pr.go
+++ cmd/verify-pr/verify-pr.go
@@ -21,24 +21,15 @@ func main() {
 	}
 
 	// Parse the event
-	event, err := func() (github.PullRequestEvent, error) {
-		eventFile, err := os.Open(ghContext.GithubEventPath)
-		if err != nil {
-			return github.PullRequestEvent{}, fmt.Errorf("unable to load event file: %w", err)
-		}
-		defer func() {
-			// As we are not writing to the file, we can omit the error
-			_ = eventFile.Close()
-		}()
-
-		var event github.PullRequestEvent
-		if err := json.NewDecoder(eventFile).Decode(&event); err != nil {
-			return event, fmt.Errorf("unable to unmarshal event: %w", err)
-		}
-		return event, nil
-	}()
+	eventFile, err := os.Open(ghContext.GithubEventPath)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal(fmt.Errorf("unable to load event file: %w", err))
+	}
+	defer eventFile.Close()
+
+	var event github.PullRequestEvent
+	if err := json.NewDecoder(eventFile).Decode(&event); err != nil {
+		log.Fatal(fmt.Errorf("unable to unmarshal PullRequest event: %w", err))
 	}
 
 	// Check the title of the PR
